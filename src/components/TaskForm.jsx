cdf6bb61ae6ef5c988b727594daa82791a9543f8
Kanav chauhan <kanavchauhan23@gmail.com>
2026-03-06 10:28:02 +0530

    Update TaskForm.jsx

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useTaskContext } from '../context/TaskContext';
 
 const CATEGORIES = ['Work','Study','Personal','Projects'];
